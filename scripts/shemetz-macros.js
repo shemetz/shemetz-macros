b83c45e3575a46c2f10ea51ff69d5ace34e87c5a
itamar <itamarcu@gmail.com>
2021-08-21 18:54:54 +0300

    Refactor and add sound macros

--- scripts/shemetz-macros.js
+++ scripts/shemetz-macros.js
@@ -1,15 +1,17 @@
+import { getDependency } from './utils/data-utils.js'
 import { bold, chat, error, italicize } from './utils/message-utils.js'
 import { crit, critDialog } from './dnd5e/crit.js'
 import { colorPickFromCursor } from './drawing/eyedropper-color-pick.js'
 import { showNamesOrBarsDialog } from './tokens/show-names-or-bars.js'
-import { getDependency } from './utils/data-utils.js'
+import { playSound, playSoundFromDialog, soundCheck } from './sound/play-sound.js'
 
 self.ShemetzMacros = {
+  getDependency,
   error, chat, italicize, bold,
   crit, critDialog,
   colorPickFromCursor,
   showNamesOrBarsDialog,
-  getDependency,
+  playSound, soundCheck, playSoundFromDialog
 }
 
 Hooks.once('init', function () {
---
+++ scripts/sound/play-sound.js
@@ -0,0 +1,49 @@
+import { SOUND_COLLECTION } from './sound-collection.js'
+import { error } from '../utils/message-utils.js'
+import { showDialogWithOptions } from '../utils/dialog-utils.js'
+
+/**
+ * used for checking that all sounds have a good volume
+ */
+export const soundCheck = async () => {
+  console.log(`SOUND CHECK`)
+  for (const soundType of Object.keys(SOUND_COLLECTION)) {
+    for (const sound of SOUND_COLLECTION[soundType]) {
+      const [volume, src] = sound
+      // play, print, and wait 2 seconds
+      console.log(`playing from ${soundType}: ${volume}, ${src}`)
+      AudioHelper.play({ src: src, volume: volume, autoplay: true, loop: false }, false)
+      await new Promise(resolve => setTimeout(resolve, 2 * 1000))
+    }
+  }
+}
+
+/**
+ * Plays one of the sounds predefined in sound-collection.js.
+ * Each possible soundType (e.g. "shield_bash", "punch") has a list of possible sounds to choose from, one of which will be picked.
+ */
+export const playSound = (soundType, playForEveryone) => {
+  console.log(`SOUND CHECK`)
+  const soundsArray = SOUND_COLLECTION[soundType]
+  if (soundsArray === undefined || !soundsArray.length) return error(
+    `${soundType} (1st arg) is not listed in the SOUND_COLLECTION array of play-sound as an array!`)
+
+  const sound = soundsArray[Math.floor(Math.random() * soundsArray.length)]
+  const [volume, src] = sound
+  AudioHelper.play({ src: src, volume: volume, autoplay: true, loop: false }, playForEveryone)
+}
+
+export const playSoundFromDialog = () => {
+  const soundTypes = ['SOUND CHECK', ...Object.keys(SOUND_COLLECTION)]
+  showDialogWithOptions(
+    'Play Sound',
+    'Choose sound type (will be played for everyone)',
+    (soundType) => {
+      if (soundType === 'SOUND CHECK')
+        soundCheck()
+      else
+        playSound(soundType, true)
+    },
+    soundTypes,
+  )
+}
