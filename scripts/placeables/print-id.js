e7cca16a2eb7792c8ca565cf200a8143ea29edbd
itamar <itamarcu@gmail.com>
2021-08-25 20:09:56 +0300

    Improve printIdOfControlled

--- scripts/placeables/print-id.js
+++ scripts/placeables/print-id.js
@@ -1,11 +1,16 @@
 /**
  * Creates a private chat message with the ID of the currently selected token/tile/wall.
  */
-export const printIdOfControlled = () => {
-  const controlled = canvas.tokens.controlled[0]
-    || canvas.background.controlled[0]
-    || canvas.foreground.controlled[0]
-    || canvas.walls.controlled[0]
-  const id = controlled ? controlled.id : 'no tile/token/wall selected'
-  ChatMessage.create({ content: id, whisper: [game.user.id] })
+export const printIdOfControlled = (onlyToConsole) => {
+  const messages = []
+    .concat(...canvas.tokens.controlled.map(tok => `token ${tok.id} (${tok.name})`))
+    .concat(...canvas.background.controlled.map(tile => `tile ${tile.id} (${tile.data.img})`))
+    .concat(...canvas.foreground.controlled.map(tile => `tile ${tile.id} (${tile.data.img})`))
+    .concat(...canvas.walls.controlled.map(wall => `wall ${wall.id}`))
+  let message = 'Printing all controlled things:\n' + messages.join('\n')
+  if (messages.length === 0)
+    message = `You should select tokens/tiles/walls before using this macro!`
+  console.info(message)
+  if (!onlyToConsole)
+    ChatMessage.create({ content: message, whisper: [game.user.id] })
 }
