deef110e9bb4480390eebd9e6c5627fd087baf39
itamar <itamarcu@gmail.com>
2021-09-05 16:34:46 +0300

    Improve postTokenArt to have the image link to full size version

--- scripts/tokens/token-art.js
+++ scripts/tokens/token-art.js
@@ -1,7 +1,10 @@
 export const postTokenArt = (tok) => {
   let target = tok.actor || tok
+  const imgSrc = target.data.img
   const content = `
-  <img src=${target.data.img} style="width: 280px;" alt=${target.name}/>
+  <a href=${imgSrc} target="_blank">
+    <img src=${imgSrc} style="width: 280px;" alt=${target.name}/>
+  </a>
   `
   ChatMessage.create({ content })
 }
