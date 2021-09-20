ca5565d69b4f9f1a9d9a24a2d768946bcf01a493
itamar <itamarcu@gmail.com>
2021-09-21 01:34:46 +0300

    Simplify Wild Surge

--- scripts/custom-stuff/wild-surge.js
+++ scripts/custom-stuff/wild-surge.js
@@ -10,25 +10,8 @@ export const castRandomWildSurge = async (actor) => {
         return
     }
     const surgeRoll = await new Roll("1d8").roll()
-    const chatData = {
-        title: `Wild Surge!`,
-        formula: "1d8",
-        tooltips: [await surgeRoll.getTooltip()],
-        rolls: [surgeRoll],
-        rollState: null, // can also be "highest" or "lowest"
-        rollType: ""
-    }
-    // TODO - not sure this is the right way
-    const multiroll = await renderTemplate("modules/betterrolls5e/templates/red-multiroll.html", chatData);
-    const messageSpeaker = ChatMessage.getSpeaker({actor: actor})
-    const messageType = CONST.CHAT_MESSAGE_TYPES.OTHER
-    ChatMessage.create({
-        speaker: messageSpeaker,
-        content: multiroll,
-        type: messageType
-    })
     const surge = surges[parseInt(surgeRoll.result) - 1]
-    BetterRolls.quickRollByName(actor.name, surge.name)
+    return BetterRolls.quickRollByName(actor.name, surge.name)
 }
 
 
