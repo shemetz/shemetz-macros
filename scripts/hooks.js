de4a79b986e33e55249aa112d8d3ea76060425b4
itamar <itamarcu@gmail.com>
2021-12-31 17:03:31 +0200

    Remove F for Flatfooted shortcut as the real module is better

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -1,7 +1,6 @@
 import { hookShemetzMacros } from './shemetz-macros.js'
 import { hookCloseWallGaps } from './walls/close-wall-gaps.js'
 import { hookEyedropperColorPicker } from './drawing/eyedropper-color-pick.js'
-import { hookPf2eFlatfootedShortcut } from './pf2e/flatfooted-shortcut.js'
 import { hookBlockPushing } from './custom-stuff/block-pushing.js'
 
 Hooks.on('init', () => {
@@ -10,10 +9,6 @@ Hooks.on('init', () => {
   hookEyedropperColorPicker()
   hookBlockPushing()
 
-  if (game.system.id === 'pf2e') {
-    hookPf2eFlatfootedShortcut()
-  }
-  
   // TODO move this to better spot and reformat it, when I get my IDE back
   // while ctrl key is held, dragged tokens will not animate and thus won't see between walls they "move" through
   Hooks.on('preUpdateToken', (doc, diff, options, userId) => {
@@ -26,8 +21,3 @@ Hooks.on('init', () => {
 		}
 	});
 })
-
-// TODO create big file for key shortcuts, based on:
-// https://github.com/Drental/foundryvtt-pf2e-f-is-for-flatfooted/blob/master/scripts/index.js
-// e.g. "T for Targeting", "E for Eyedropper", "F for Flip"
-// avoid: WASD, C
