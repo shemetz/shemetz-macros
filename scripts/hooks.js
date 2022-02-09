6df8d8631362da60154653a235687cf0ab536384
itamar <itamarcu@gmail.com>
2022-02-09 02:10:30 +0200

    Removed automatic wound effects because it's its own module now
    https://github.com/itamarcu/tokenmagic-automatic-wounds

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -2,7 +2,6 @@ import { hookShemetzMacros } from './shemetz-macros.js'
 import { hookCloseWallGaps } from './walls/close-wall-gaps.js'
 import { hookEyedropperColorPicker } from './drawing/eyedropper-color-pick.js'
 import { hookBlockPushing } from './custom-stuff/block-pushing.js'
-import { hookAutomaticWoundEffects, registerAutomaticWoundEffects } from './tokens/automatic-wound-effects.js'
 import { hookControlTokenTeleportation } from './tokens/control-token-teleportation.js'
 import { hookHiddenHotkey } from './placeables/toggle-hide.js'
 import { hookImageShiftHotkey } from './placeables/image-shift.js'
@@ -14,14 +13,10 @@ Hooks.on('init', () => {
   hookCloseWallGaps()
   hookEyedropperColorPicker()
   hookBlockPushing()
-  registerAutomaticWoundEffects()
   hookControlTokenTeleportation()
   hookHiddenHotkey()
   hookImageShiftHotkey()
   hookLayerSwitchHotkey()
   hookRepeatLatestOperation()
   hookRepeatLatestOperationHotkey()
-})
-Hooks.on('ready', () => {
-  hookAutomaticWoundEffects()
 })
