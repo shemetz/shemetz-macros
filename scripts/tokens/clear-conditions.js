b2ef89680e03ae85e2575e5bb92cb842f3a67b45
itamar <itamarcu@gmail.com>
2022-08-03 09:39:43 +0300

    Fixed unnecessary await in clearAllConditions that slowed it down

--- scripts/tokens/clear-conditions.js
+++ scripts/tokens/clear-conditions.js
@@ -6,13 +6,13 @@ export const clearAllConditions = async (tokens) => {
   const promises = []
   for (const tok of tokens) {
     updates.push({ _id: tok.id, effects: [], overlayEffect: '' })
-    // Status Icon Counters:
+    // Status Icon Counters module:
     if (self.EffectCounter) promises.push(self.EffectCounter.clearEffects(tok.document))
     // Pathfinder 2e conditions:
     if (game.system.id === 'pf2e') {
-      promises.push(await game.pf2e.ConditionManager.deleteConditions(tok.actor.itemTypes.condition.map(c => c.id), tok.actor))
+      promises.push(game.pf2e.ConditionManager.deleteConditions(tok.actor.itemTypes.condition.map(c => c.id), tok.actor))
       // promises.push(await game.pf2e.effectTracker.removeExpired(tok.actor))
-      promises.push(await tok.actor.deleteEmbeddedDocuments('Item', tok.actor.itemTypes.effect.filter(e => e.data.data.tokenIcon.show).map(e => e.id)))
+      promises.push(tok.actor.deleteEmbeddedDocuments('Item', tok.actor.itemTypes.effect.filter(e => e.system.tokenIcon.show).map(e => e.id)))
     }
   }
   await Promise.all(promises)
