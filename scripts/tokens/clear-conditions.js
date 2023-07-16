b82510374017e076dac5de2a57bbd036c8f0ccef
itamar <itamarcu@gmail.com>
2023-07-16 10:55:07 +0300

    Updated clear-conditions

--- scripts/tokens/clear-conditions.js
+++ scripts/tokens/clear-conditions.js
@@ -3,37 +3,16 @@
  */
 export const clearAllConditions = async (tokens) => {
   const updates = []
-  const promises = []
   for (const tok of tokens) {
     updates.push({ _id: tok.id, effects: [], overlayEffect: '' })
     // Status Icon Counters module:
     if (self.EffectCounter) promises.push(self.EffectCounter.clearEffects(tok.document))
     // Pathfinder 2e conditions:
     if (game.system.id === 'pf2e' && !!tok.actor) {
-      promises.push(pf2eDeleteConditions(tok.actor.itemTypes.condition.map(c => c.id), tok.actor))
-      // promises.push(await game.pf2e.effectTracker.removeExpired(tok.actor))
-      promises.push(tok.actor.deleteEmbeddedDocuments('Item',
-        tok.actor.itemTypes.effect.filter(e => e.system.tokenIcon.show).map(e => e.id)))
+      const ids = tok.actor.itemTypes.condition.map(x => x.id)
+      ids.push(...tok.actor.itemTypes.effect.map(x => x.id))
+      await tok.actor.deleteEmbeddedDocuments('Item', ids)
     }
   }
-  await Promise.all(promises)
   return canvas.scene.updateEmbeddedDocuments('Token', updates)
-}
-
-const pf2eDeleteConditions = async (itemIds, actor) => {
-  const ConditionPF2e = CONFIG.PF2E.Item.documentClasses.condition
-
-  const list = []
-  const stack = [...itemIds]
-  while (stack.length) {
-    const id = stack.pop() ?? ''
-    const condition = actor.items.get(id)
-
-    if (condition instanceof ConditionPF2e) {
-      list.push(id)
-      condition.data.data.references.children.forEach((child) => stack.push(child.id))
-    }
-  }
-
-  return ConditionPF2e.deleteDocuments(list, { parent: actor })
 }
