fd127e56c90ed2402c2d2762b6170c5cc616052f
itamar <itamarcu@gmail.com>
2024-06-22 18:16:05 +0300

    Refactor H keybind (toggle hidden)

--- scripts/placeables/toggle-hide.js
+++ scripts/placeables/toggle-hide.js
@@ -1,8 +1,8 @@
 import { error } from '../utils/message-utils.js'
 
 /**
- * state = true, false, or "flip"
- * tokenNameOrTileId  = "robert" or "sDdak6s4KJd"
+ * @param state true, false, or "flip"
+ * @param tokenNameOrTileId e.g. "robert" or "sDdak6s4KJd"
  */
 export const toggleHide = async (state, tokenNameOrTileId) => {
   if (state === undefined || tokenNameOrTileId === undefined) {
@@ -12,23 +12,36 @@ export const toggleHide = async (state, tokenNameOrTileId) => {
   if (!t) t = canvas.tiles.get(tokenNameOrTileId)
   if (!t) return error(`could not find token/tile ${tokenNameOrTileId}`)
   let newState
-  switch (true) {
-    case state === true: newState = true; break;
-    case state === false: newState = false; break;
-    case state === 'flip': newState = !t.document.hidden; break;
-    default: newState = null; break;
+  switch (state) {
+    case true:
+    case 'true':
+      newState = true
+      break
+    case false:
+    case 'false':
+      newState = false
+      break
+    case 'flip':
+      newState = !t.document.hidden
+      break
+    default:
+      return error(`invalid state: ${state} (use true, false, or "flip")`)
   }
-  if (newState === null) return error(`invalid state: ${state}`)
   return t.document.update({ 'hidden': newState })
 }
 
-const toggleVisibility = async (canvasObjectList, documentName) => {
-  if (canvasObjectList.controlled[0]) {
-    return canvas.scene.updateEmbeddedDocuments(
-      documentName,
-      canvasObjectList.controlled.map(it => ({ _id: it.id, hidden: !it.document.hidden }))
-    )
-  } else return false
+const onToggleHiddenPressed = async () => {
+  const controlledPlaceables = canvas.activeLayer?.controlled
+  if (controlledPlaceables?.length <= 0)
+    return
+  const documentName = canvas.activeLayer.documentCollection.documentClass.documentName
+  if (controlledPlaceables[0].document.hidden === undefined)
+    return ui.notifications.error(`${documentName} does not have a 'hidden' property, so this keybind won't work.`)
+
+  return canvas.scene.updateEmbeddedDocuments(
+    documentName,
+    controlledPlaceables.map(it => ({ _id: it.id, hidden: !it.document.hidden })),
+  )
 }
 
 export const hookHiddenHotkey = () => {
@@ -36,14 +49,8 @@ export const hookHiddenHotkey = () => {
     name: 'Toggle Hidden',
     hint: 'Toggle the visibility state of each selected token, tile, or drawing (hiding or revealing it).',
     editable: [
-      { key: 'KeyH' }
+      { key: 'KeyH' },
     ],
-    onDown: async () => {
-      // only one of the following will happen, because the user can only be on a single layer
-      return await toggleVisibility(canvas.tokens, 'Token') ||
-      await toggleVisibility(canvas.drawings, 'Drawing') ||
-      await toggleVisibility(canvas.tiles, 'Tile') ||
-      false
-    },
+    onDown: onToggleHiddenPressed,
   })
 }
