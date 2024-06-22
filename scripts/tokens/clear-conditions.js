837f66e2339d40fd95e10e840063b349eff6753d
itamar <itamarcu@gmail.com>
2024-06-22 19:12:50 +0300

    Fixed clear-conditions again

--- scripts/tokens/clear-conditions.js
+++ scripts/tokens/clear-conditions.js
@@ -2,17 +2,20 @@
  Clear all conditions from tokens.
  */
 export const clearAllConditions = async (tokens) => {
-  const updates = []
   for (const tok of tokens) {
-    updates.push({ _id: tok.id, effects: [] })
+    if (!tok.actor) continue
     // Status Icon Counters module:
     if (self.EffectCounter) promises.push(self.EffectCounter.clearEffects(tok.document))
-    // Pathfinder 2e conditions:
-    if (game.system.id === 'pf2e' && !!tok.actor) {
+    // Pathfinder 2e conditions:  split into two, and also include automatic things we don't want to remove
+    if (game.system.id === 'pf2e') {
       const ids = tok.actor.itemTypes.condition.map(x => x.id)
       ids.push(...tok.actor.itemTypes.effect.map(x => x.id))
-      await tok.actor.deleteEmbeddedDocuments('Item', ids)
+      if (ids.length > 0)
+        await tok.actor.deleteEmbeddedDocuments('Item', ids)
+    } else {
+      const ids = tok.actor.effects.filter(e => e.statuses?.size > 0).map(e => e.id)
+      if (ids.length > 0)
+        await tok.actor.deleteEmbeddedDocuments('ActiveEffect', ids)
     }
   }
-  return canvas.scene.updateEmbeddedDocuments('Token', updates)
 }
